ce97b1862f64ecce5e7bfb7018c54eef7c3b6254
Alabhya Jindal <alabhya10@gmail.com>
2022-03-21 14:54:53 +0530

    player's movememt is working

--- app.js
+++ app.js
@@ -26,46 +26,45 @@ for (let i = 0; i < playerCount; i++) {
   startSquare.appendChild(player);
 }
 
-// // Selecting the dice roll button
-// const roll = document.querySelector(".btn-roll");
-// // Selecting the element to display rolling dice's results
-// const result = document.querySelector(".roll-result");
+// Selecting the dice roll button
+const roll = document.querySelector(".btn-roll");
+// Selecting the element to display rolling dice's results
+const result = document.querySelector(".roll-result");
 
-// // Function to get a random dice outcome
-// const rollingDice = function () {
-//   result.style.color = "#333";
-//   result.textContent = Math.floor(Math.random() * 6) + 1;
-// };
+// Function to get a random dice outcome
+const rollingDice = function () {
+  result.style.color = "#333";
+  result.textContent = Math.floor(Math.random() * 6) + 1;
+};
 
 // // Nodelist of player pieces
 // const playerPieces = document.querySelectorAll(".player");
 
-// // Selecting the first player element to be moved
-// const firstPlayer = document.querySelector(".player-1");
+// Selecting the first player element to be moved
+const firstPlayer = document.querySelector(".player-1");
 
-// // Function to move the players based on the result of the dice
-// const move = function (dice) {
-//   const initialSquare = document.querySelector(
-//     `.square-${currentPosition() + dice}`
-//   );
-//   console.log(initialSquare);
-//   initialSquare.appendChild(firstPlayer);
-// };
+// Function to move the players based on the result of the dice
+const move = function (dice) {
+  const initialSquare = document.getElementById(currentPosition() + dice);
 
-// // Rolling the dice on the click of the button
-// roll.addEventListener("click", () => {
-//   rollingDice();
-//   const result = document.querySelector(".roll-result");
-//   move(parseInt(result.textContent));
-// });
+  console.log(currentPosition() + dice);
+  initialSquare.appendChild(firstPlayer);
+};
 
-// // Selecting the parent which has the player piece child element
-// const currentPosition = function () {
-//   for (let i = 0; i < 100; i++) {
-//     if (squareList[i].children.length > 0) {
-//       return parseInt(squareList[i].classList[2]);
-//     }
-//   }
-// };
+// Rolling the dice on the click of the button
+roll.addEventListener("click", () => {
+  rollingDice();
+  const result = document.querySelector(".roll-result");
+  move(parseInt(result.textContent));
+});
 
-// currentPosition();
+// Selecting the parent which has the player piece child element
+const currentPosition = function () {
+  for (let i = 0; i < 100; i++) {
+    if (squareList[i].children.length > 0) {
+      return parseInt(squareList[i].getAttribute("id"));
+    }
+  }
+};
+
+currentPosition();
